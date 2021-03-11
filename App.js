402a089736329dedc439b2b4fed9b8bc8a2614e7
Jeorge Donato <jeorgekhenr@gmail.com>
2021-03-11 08:44:00 -0800

    First commit

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
-export default function App() {
+const { useState } = React
+
+export default function App () {
+  const [counter, setCounter] = useState(0)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>{counter}</Text>
+      <StatusBar style='auto' backgroundColor='red' />
+      <Button title='Increase Counter' onPress={() => { setCounter(counter + 1) }} />
+      <Button title='Decrease Counter' onPress={() => { setCounter(counter - 1) }} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +23,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexWrap: 'wrap'
   },
-});
+  text: {
+    borderStyle: 'solid',
+    borderColor: 'green',
+    borderWidth: '2px'
+  }
+})
